5ef521b8186ad47a511a4995552817e869c4bb5c
aish9486 <144314003+aish9486@users.noreply.github.com>
2023-09-19 13:52:25 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,19 +1,29 @@
-# Python3 implementation to check
-# if the year is a leap year
-# using macros
- 
-# Macro to check if a year
-# is a leap year
-def ISLP(y):
-  if((y % 400 == 0) or
-     (y % 100 != 0) and
-     (y % 4 == 0)):
-    return 1;
-  else:
-    return 0;
- 
-# Driver code
-if __name__=='__main__':
- 
-  year = 2020;
-  print(ISLP(year));
+'''Implement a class called Player that represents a cricket player. The Player class should have a
+method called play() which prints "The player is playing cricket. Derive two classes, Batsman and
+Bowler, from the Player class. Override the play() method in each derived class to print "The batsman
+is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the
+Batsman and Bowler classes and call the play() method for each object.'''
+
+
+# Define the base class Player
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+
+# Define the derived class Batsman
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+# Define the derived class Bowler
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
